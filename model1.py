46b548723d963ead41583d320f896e34c49a8f35
Pablo Jadzinsky <pjadzinsky@gmail.com>
2015-08-01 11:27:11 -0700

    Small improvement RNN works with two inputs (stim and spikes) but to make it compile and run, instead of concatenating I'm summing the two inputs.

--- model1.py
+++ model1.py
@@ -2,14 +2,14 @@ from load_cell import Cell
 from keras.models import Graph
 from keras.layers.core import Activation, Reshape
 from keras.activations import linear, relu
-from keras.layers.recurrent import LSTM
+from keras.layers.recurrent import LSTM, SimpleRNN
 from keras.optimizers import RMSprop, SGD
 
 class model():
     def __init__(self):
         graph = Graph()
         graph.add_input(name='stim', ndim=3)
-        #graph.add_input(name='spikes', ndim=3)
+        graph.add_input(name='spikes', ndim=3)
 
         """
         graph.add_node(Reshape(100, 1),
@@ -18,15 +18,15 @@ class model():
                 name='spikes_3D', input='spikes')
         """
 
-        #graph.add_node(Activation(linear), 
-        #        name='concat', inputs=['stim', 'spikes'], merge_mode='concat')
         graph.add_node(Activation(linear), 
-                name='concat', input='stim')
+                name='concat', inputs=['stim', 'spikes'], merge_mode='sum')
+        #graph.add_node(Activation(linear), 
+        #        name='concat', input='stim')
 
-        graph.add_node(LSTM(1, 1, return_sequences=True),
-                name='LSTM', input='concat')
+        graph.add_node(SimpleRNN(1, 1, return_sequences=True),
+                name='SimpleRNN', input='concat')
 
-        graph.add_output(name='mp', input='LSTM')
+        graph.add_output(name='mp', input='SimpleRNN')
         
         graph.compile('rmsprop', {'mp':'mse'})
 
